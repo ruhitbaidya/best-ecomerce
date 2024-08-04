c1ac31e7cd33cb6648e4fb5e58463690b8f7d036
ruhitbaidya <ruhitbaidya01@gmail.com>
2024-08-04 10:20:56 +0600

    city data loading finish

--- src/Pages/Dashboard/BottomSilder/BottomSection.jsx
+++ src/Pages/Dashboard/BottomSilder/BottomSection.jsx
@@ -1,7 +1,17 @@
 import { Link } from "react-router-dom";
 import BottomSlider from "./BottomSlider";
+import { useEffect, useState } from "react";
 
 const BottomSection = () => {
+  const [cityName, setCityname] = useState(null)
+  useEffect(()=>{
+    fetch('CityName.json')
+    .then((res)=> res.json())
+    .then((data)=> {
+      console.log(data)
+      setCityname(data)
+    })
+  }, [])
   return (
     <div className="mt-[20px] lg:flex items-center gap-[25px]">
       <div className="flex-1">
@@ -13,24 +23,11 @@ const BottomSection = () => {
       <div className="flex-1">
         <h2 className="text-[20px] font-bold">Popular Cities</h2>
         <div className="bg-white p-[20px] mt-[20px] rounded-lg">
-            <div className="grid grid-cols-3 gap-[20px] text-center">
-                <p className="border"><Link to="">Atlanta, GA</Link></p>
-                <p><Link to="">Atlanta, GA</Link></p>
-                <p><Link to="">Atlanta, GA</Link></p>
-                <p><Link to="">Atlanta, GA</Link></p>
-                <p><Link to="">Atlanta, GA</Link></p>
-                <p><Link to="">Atlanta, GA</Link></p>
-                <p><Link to="">Atlanta, GA</Link></p>
-                <p><Link to="">Atlanta, GA</Link></p>
-                <p><Link to="">Atlanta, GA</Link></p>
-                <p><Link to="">Atlanta, GA</Link></p>
-                <p><Link to="">Atlanta, GA</Link></p>
-                <p><Link to="">Atlanta, GA</Link></p>
-                <p><Link to="">Atlanta, GA</Link></p>
-                <p><Link to="">Atlanta, GA</Link></p>
-                <p><Link to="">Atlanta, GA</Link></p>
-                <p><Link to="">Atlanta, GA</Link></p>
-                <p><Link to="">Atlanta, GA</Link></p>
+            <div className="grid grid-cols-3  text-center ">
+              {
+                cityName?.map((item)=> <p key={item.id} className="row-item text-[#156BCA] underline p-[20px]"><Link to="">{item.cityName}</Link></p>)
+              }
+                
             </div>
         </div>
       </div>
