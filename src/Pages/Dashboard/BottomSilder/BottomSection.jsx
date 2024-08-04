208ded762cacfb455bb0b4c43ba8e46a3a62c0d0
ruhitbaidya <ruhitbaidya01@gmail.com>
2024-08-05 01:39:47 +0600

    finish work

--- src/Pages/Dashboard/BottomSilder/BottomSection.jsx
+++ src/Pages/Dashboard/BottomSilder/BottomSection.jsx
@@ -16,16 +16,16 @@ const BottomSection = () => {
     <div className="mt-[20px] lg:flex items-center gap-[25px]">
       <div className="flex-1">
         <h2 className="text-[20px] font-bold">Featured Testimonial</h2>
-        <div className="mt-[20px] bg-white p-[20px] rounded-lg">
+        <div className="mt-[20px] bg-white py-[50px] rounded-lg">
           <BottomSlider></BottomSlider>
         </div>
       </div>
       <div className="flex-1">
         <h2 className="text-[20px] font-bold">Popular Cities</h2>
         <div className="bg-white p-[20px] mt-[20px] rounded-lg">
-            <div className="grid grid-cols-2  lg:grid-cols-3  text-center ">
+            <div className="grid grid-cols-1  lg:grid-cols-3  text-center ">
               {
-                cityName?.map((item)=> <p key={item.id} className="row-item text-[#156BCA] underline p-[20px]"><Link to="">{item.cityName}</Link></p>)
+                cityName?.map((item)=> <p key={item.id} className="row-item text-[#156BCA] border-b-2 p-[15px]"><Link to="">{item.cityName}</Link></p>)
               }
                 
             </div>
--- src/Pages/Dashboard/BottomSilder/BottomSlider.jsx
+++ src/Pages/Dashboard/BottomSilder/BottomSlider.jsx
@@ -1,6 +1,4 @@
 import { Link } from "react-router-dom";
-import imge1 from "../../../assets/btslid1.png";
-import imge2 from "../../../assets/btslid2.png";
 import { FaLocationDot } from "react-icons/fa6";
 import "./stylesot.css";
 import Slider from "react-slick";
@@ -8,7 +6,7 @@ import { useEffect, useState } from "react";
 const BottomSlider = () => {
   const [data, setData] = useState(null);
   useEffect(() => {
-    fetch("Persons.json")
+    fetch("UnderPerson.json")
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -20,10 +18,10 @@ const BottomSlider = () => {
     infinite: true,
     slidesToShow: 2,
     slidesToScroll: 1,
-
     vertical: true,
     verticalSwiping: true,
     swipeToSlide: true,
+    arrows: false,
     beforeChange: function (currentSlide, nextSlide) {
       console.log("before change", currentSlide, nextSlide);
     },
@@ -35,25 +33,28 @@ const BottomSlider = () => {
     <>
       <div className="slider-container">
         <Slider {...settings}>
-          <div className=" flex border p-[30px] items-center rounded-lg mb-[10px]">
-            <div className="">
-              <img src={imge1} alt="" />
-            </div>
-            <div className=""></div>
-            <div className=" space-y-4">
-              <p className="flex items-center gap-[15px]">
-                {" "}
-                <FaLocationDot /> <span>123 Elm Street, ew York</span>{" "}
-              </p>
-              <p className="text-left font-bold">
-                Healing Bodywork <Link to="">by Cort</Link>
-              </p>
-              <p className="text-left">
-                Cort’s healing bodywork massage was absolutely transformative.
-                Their intuitive touch and deep understanding of
-              </p>
-            </div>
-          </div>
+            {
+                data?.map((item)=> <div key={item.zip_code}>
+                <div className=" flex border gap-[15px] p-[10px] items-center rounded-lg mb-[10px]">
+                  <div className="w-[40%]">
+                    <img className="h-[100%]" src={item.image} alt="" />
+                  </div>
+                  <div className="w-[60%] space-y-4">
+                    <p className="flex items-center gap-[15px]">
+                      {" "}
+                      <FaLocationDot /> <span>{item.zip_code}, {item.city}</span>{" "}
+                    </p>
+                    <p className="text-left font-bold">
+                      {item.name} <Link to="">by Cort</Link>
+                    </p>
+                    <p className="text-left">
+                      {item.description.slice(0, 30)} <Link to="" className="text-blue-500"> Read More..</Link>
+                    </p>
+                  </div>
+                </div>
+                </div> )
+            }
+          
         </Slider>
       </div>
     </>
